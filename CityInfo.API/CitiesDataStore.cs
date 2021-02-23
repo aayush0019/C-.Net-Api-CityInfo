35ab3780b1397988e5ac81f76ed33c4469b41c5f
Aayush Gupta <aayush0019@gmail.com>
2021-02-23 13:05:03 +0530

    Creating the API + Returning CitiesData

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,40 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        static public CitiesDataStore Current = new CitiesDataStore();
+
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Delhi",
+                    Description = "Capital of India"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Ahmedabad",
+                    Description = "Heritage City announced by UNESCO"
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Bangalore",
+                    Description = "Silicon Valley of India"
+                }
+            };
+        }
+    }
+}
