fa2dd1ab638c527daa2e243aab7f6bf3763daef0
Aayush Gupta <aayush0019@gmail.com>
2021-02-23 18:28:56 +0530

    Resource-Addition

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -20,19 +20,52 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "Delhi",
-                    Description = "Capital of India"
+                    Description = "Capital of India",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Taj Mahal",
+                            Description = "One of the 7 wonders of the world."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Qutub Minar",
+                            Description = "The tallest minaret in the world built of bricks."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Ahmedabad",
-                    Description = "Heritage City announced by UNESCO"
+                    Description = "Heritage City announced by UNESCO",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "Sabarmati Ashram",
+                            Description = "Mahatma Gandhiji's Museum"
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Bangalore",
-                    Description = "Silicon Valley of India"
+                    Description = "Silicon Valley of India",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "Lalbagh Botanical Garden",
+                            Description = "Old botanical garden"
+                        }
+                    }
                 }
             };
         }
